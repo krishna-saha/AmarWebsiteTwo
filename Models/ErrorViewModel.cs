c0d065ba713b309564550c8dc4ce91d354845bdd
krish_datta <krish_datta@hotmail.com>
2026-02-02 22:30:43 -0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AmarWebsite_Code.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
